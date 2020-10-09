ed6be05169dc6538ddd886772e34907006f11153
nuria1319 <nuriafernandez1319@gmail.com>
2020-10-09 13:27:00 +0100

    primera version

---
+++ Sumador.java
@@ -0,0 +1,12 @@
+public class Sumador {
+
+    public static void main(String[] args) {
+
+        int num1 = 3;
+        int num2 = 2;
+        int suma = num1 + num2;
+        System.out.println("La suma vale: " + suma);
+        
+
+    }
+}
